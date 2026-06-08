46be4dc46687ccb32ce29db70b8ecf8f1b5a3182
sh3boo <ahmedshanan2021@gmail.com>
2026-06-08 22:59:15 +0300

    AddedRepositories

---
+++ Services/Basket/Basket.Infrastructure/Repositories/BasketRepository.cs
@@ -0,0 +1,44 @@
+﻿using Basket.Core.Entites;
+using Basket.Core.Repositories;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket.Infrastructure.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+
+        private readonly IDistributedCache _redisCache;
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+
+        }
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+
+            }
+            return JsonConvert.DeserializeObject <ShoppingCart>(basket);
+
+        }
+
+        public Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteBasket(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
