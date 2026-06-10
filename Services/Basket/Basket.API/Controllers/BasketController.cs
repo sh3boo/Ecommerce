5c42a908a7a3af0789460c06233f39697911ed29
sh3boo <ahmedshanan2021@gmail.com>
2026-06-10 22:13:31 +0300

    added BasketController

---
+++ Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,25 @@
+﻿using Basket.Application.Queries;
+using Basket.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    public class BasketController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public BasketController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        [Route("[action]/{userName}", Name = "GetBasketByUserName")]
+        public async Task<ActionResult<ShoppingCartResponse>> GetBasket(string userName)
+        {
+            var query = new GetBasketByUserNameQuery(userName);
+            var basket = await _mediator.Send(query);
+            return Ok(basket);
+        }
+    }
+}
--- Services/Basket/Basket.API/Program.cs
+++ Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,11 @@
 
+using Basket.Application.Handlers.Commands;
+using Basket.Application.Mappers;
+using Basket.Core.Repositories;
+using Basket.Infrastructure.Repositories;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Basket.API
 {
     public class Program
@@ -13,12 +20,56 @@ namespace Basket.API
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            builder.Services.AddAutoMapper(cfg => cfg.AddMaps(typeof(BasketMappingProfile).Assembly));
+            //builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(
+            //    Assembly.GetExecutingAssembly(),
+            //    Assembly.GetAssembly(typeof(ProductMappingProfile))
+
+            //    ));
+            builder.Services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(CreateShoppingCartCommand).Assembly);
+            });
+
+            builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+            builder.Services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new Asp.Versioning.ApiVersion(1, 0);
+            });
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.OpenApiInfo
+                {
+                    Title = "Basket.API",
+                    Version = "v1",
+                    Description = "Basket Microservice API",
+                    Contact = new Microsoft.OpenApi.OpenApiContact
+                    {
+                        Name = "Ahemd Shaban",
+                        Email = "ahmedshanan2021@gmail.com"
+                    }
+                }
+                    );
+            });
+
+            builder.Services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionStrings");
+            });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseAuthorization();
