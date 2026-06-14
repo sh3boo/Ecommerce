d80b46aad82e4b8ca0684906534341c4d0efcc81
Ahmed Shaban <101558920+sh3boo@users.noreply.github.com>
2026-06-14 10:21:32 +0300

    update Basket

--- Services/Basket/Basket.API/Program.cs
+++ Services/Basket/Basket.API/Program.cs
@@ -1,10 +1,8 @@
-
 using Basket.Application.Handlers.Commands;
 using Basket.Application.Mappers;
 using Basket.Core.Repositories;
 using Basket.Infrastructure.Repositories;
-using MediatR;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace Basket.API
 {
@@ -15,17 +13,11 @@ namespace Basket.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
 
-            builder.Services.AddAutoMapper(cfg => cfg.AddMaps(typeof(BasketMappingProfile).Assembly));
-            //builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(
-            //    Assembly.GetExecutingAssembly(),
-            //    Assembly.GetAssembly(typeof(ProductMappingProfile))
+            builder.Services.AddAutoMapper(cfg =>
+                cfg.AddMaps(typeof(BasketMappingProfile).Assembly));
 
-            //    ));
             builder.Services.AddMediatR(cfg =>
             {
                 cfg.RegisterServicesFromAssembly(typeof(CreateShoppingCartCommand).Assembly);
@@ -39,41 +31,43 @@ namespace Basket.API
                 options.AssumeDefaultVersionWhenUnspecified = true;
                 options.DefaultApiVersion = new Asp.Versioning.ApiVersion(1, 0);
             });
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
             builder.Services.AddEndpointsApiExplorer();
+
             builder.Services.AddSwaggerGen(options =>
             {
-                options.SwaggerDoc("v1", new Microsoft.OpenApi.OpenApiInfo
+                options.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Title = "Basket.API",
                     Version = "v1",
                     Description = "Basket Microservice API",
-                    Contact = new Microsoft.OpenApi.OpenApiContact
+                    Contact = new OpenApiContact
                     {
-                        Name = "Ahemd Shaban",
-                        Email = "ahmedshanan2021@gmail.com"
+                        Name = "Ahmed Shaban",
+                        Email = "ahmedshaban2021@gmail.com"
                     }
-                }
-                    );
+                });
             });
 
             builder.Services.AddStackExchangeRedisCache(options =>
             {
-                options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionStrings");
+                options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
             });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
             {
-                app.MapOpenApi();
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.API v1");
+                options.RoutePrefix = "swagger";
+            });
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
