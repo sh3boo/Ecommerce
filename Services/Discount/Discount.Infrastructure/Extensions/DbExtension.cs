ec42bef34c4452460688015919f82a3be13b1888
sh3boo <ahmedshanan2021@gmail.com>
2026-08-24 07:04:13 +0300

    update Discount

--- Services/Discount/Discount.Infrastructure/Extensions/DbExtension.cs
+++ Services/Discount/Discount.Infrastructure/Extensions/DbExtension.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +14,68 @@ namespace Discount.Infrastructure.Extensions
 {
     public static class DbExtension
     {
+        public static IHost MigrateDatabase<TContext>(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var Services = scope.ServiceProvider;
+                var config = Services.GetRequiredService<IConfiguration>();
+                var logger = Services.GetRequiredService<Logger<TContext>>();
+                try
+                {
+                    logger.LogInformation("Discount DB Migration Started");
+                    ApplyMigrations(config);
+                    logger.LogInformation("Discount DB Migration completed");
+
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError("Can't create db migration");
+                }
+            }
+            return host;
+        }
+        private static void ApplyMigrations(IConfiguration config) 
+        {
+            var retry = 5;
+            while (retry-- > 0)
+            {
+                try
+                {
+                    using var connection = new NpgsqlConnection(config.GetValue<string>("DatabaseSettings:Connection"));
+                    connection.Open();
+                    using var cmd = new NpgsqlCommand
+                    {
+                        Connection = connection
+                    };
+                    cmd.CommandText = "Drop Table IF EXISTs Coupon";
+                    cmd.ExecuteNonQuery();
+                    cmd.CommandText = (@"Create Table Coupon (Id Serial Primary key,
+                                        ProductName Nvarchar(500)Not Null,
+                                        Description Text,
+                                        Amount INT)");
+                    cmd.ExecuteNonQuery();
+                    cmd.CommandText = @"Insert Into Coupon (ProductName , Desctiption , Amount)
+                                                            Values ('Egypt Adidas Quick Force Indoor Badminton Shoes','Addidas Discount',600);";
+                    cmd.ExecuteNonQuery();
+                    cmd.CommandText = @"Insert Into Coupon (ProductName , Desctiption , Amount)
+                                                            Values ('PowerFit 19 FH Rubber Spike Cricket Shoes','PowerFit Discount',600);";
+                    cmd.ExecuteNonQuery();
+
+
+                }
+                catch (Exception ex)
+                {
+                    retry--;
+                    if (retry ==0)
+                    {
+                        throw;
+                    }
+                    Thread.Sleep(2000);
+
+
+                }
+            }
+        }
     }
 }
