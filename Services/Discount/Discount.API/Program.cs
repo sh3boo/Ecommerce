821fb0b8b97ca507cd564c65c764294ca6d3da83
sh3boo <ahmedshanan2021@gmail.com>
2026-08-25 07:04:48 +0300

    added updates

--- Services/Discount/Discount.API/Program.cs
+++ Services/Discount/Discount.API/Program.cs
@@ -1,4 +1,10 @@
 
+using Discount.API.Services;
+using Discount.Application.Commands;
+using Discount.Application.Mapper;
+using Discount.Core.Repositories;
+using Discount.Infrastructure.Extensions;
+
 namespace Discount.API
 {
     public class Program
@@ -13,18 +19,40 @@ namespace Discount.API
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            builder.Services.AddAutoMapper(cfg =>
+             cfg.AddMaps(typeof(DiscountProfile).Assembly));
+
+            builder.Services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(CreateDiscountCommand).Assembly);
+            });
+
+            builder.Services.AddScoped<IDiscountRepository, IDiscountRepository>();
+            builder.Services.AddGrpc();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+
+                //app.MapOpenApi();
+                app.UseDeveloperExceptionPage();
             }
+            app.MigrateDatabase<Program>();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => 
+            {
+                endpoints.MapGrpcService<DiscountService>();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with GRPC Endpoints must be made throw a grpc client");
+                });
+            } );
+            //app.UseAuthorization();
 
-            app.UseAuthorization();
 
-
-            app.MapControllers();
+            //app.MapControllers();
 
             app.Run();
         }
---
+++ Services/Discount/Discount.API/Services/DiscountService.cs
@@ -0,0 +1,63 @@
+﻿using MediatR;
+using Discount.Grpc.Protos;
+
+
+
+using Grpc.Core;
+using Discount.Application.Queries;
+using Discount.Application.Commands;
+
+namespace Discount.API.Services
+{
+    public class DiscountService:DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IMediator _mediator;
+        public DiscountService(IMediator mediator)
+        {
+            _mediator = mediator;
+            
+        }
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var query = new GetDiscountQuery(request.ProductName);
+            var result = await _mediator.Send(query);
+            return result;
+        }
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            var cmd = new CreateDiscountCommand
+            {
+                ProductName = request.Coupon.ProductName,
+                Amount = request.Coupon.Amount,
+                Description = request.Coupon.Description
+            };
+            var res = await _mediator.Send(cmd);
+            return res;
+        }
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            var cmd = new UpdateDiscountCommand
+            {
+                ProductName = request.Coupon.ProductName,
+                Amount = request.Coupon.Amount,
+                Description = request.Coupon.Description,
+                Id = request.Coupon.Id
+            };
+            var res = await _mediator.Send(cmd);
+            return res;
+        }
+
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            var cmd = new DeleteDiscountCommand(request.ProductName);
+            var deleted = await _mediator.Send(cmd);
+            var response = new DeleteDiscountResponse
+            {
+                Success = deleted
+            };
+                return response;
+        }
+
+    }
+}
