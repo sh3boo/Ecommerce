030bf24793732ad1ab61a4da4e814ccd7eda3d8d
Ahmed Shaban <101558920+sh3boo@users.noreply.github.com>
2026-09-13 15:49:29 +0300

    update Ordering

---
+++ Services/Ordering/Ordering.Infrastructure/Data/OrderContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextFactory : IDesignTimeDbContextFactory<OrderContext>
+    {
+        public OrderContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<OrderContext>();
+            optionsBuilder.UseSqlServer("Server=localhost;Database=OrderingDb;User Id=sa;Password=Password123;TrustServerCertificate=True;");
+
+            return new OrderContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Services/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Core.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext,ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seeded database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        public static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order
+                {
+                    UserName = "Sh3boo",
+                    FirstName = "Sh3boo",
+                    LastName = "Sh3boo",
+                    EmailAddress = "sh3boo@example.com"
+                }
+
+            };
+        }
+    }
+}
