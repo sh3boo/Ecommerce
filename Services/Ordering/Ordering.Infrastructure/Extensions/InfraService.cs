061d49b8968c17183c7bb34deef10882d90a8f7d
Ahmed Shaban <101558920+sh3boo@users.noreply.github.com>
2026-09-14 08:50:23 +0300

    added the infraservice

---
+++ Services/Ordering/Ordering.Infrastructure/Extensions/InfraService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Core.Repositories;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repositories;
+
+
+namespace Ordering.Infrastructure.Extensions
+{
+    public static class InfraService
+    {
+        public static IServiceCollection AddInfraService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrderContext>(options => options.UseSqlServer(
+                configuration.GetConnectionString("OrderingConnectionString"),
+                SqlServerOptions => SqlServerOptions.EnableRetryOnFailure()
+
+                )
+            );
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            return services;
+        }
+    }
+}
